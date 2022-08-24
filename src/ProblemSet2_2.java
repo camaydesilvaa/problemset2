2022999b754376ae537ea02ca6ee53556f193d81
camaydesilvaa <camay.desilva@cdpipelinedevshop.com>
2022-08-24 12:34:53 -0400

    First Commit

---
+++ src/ProblemSet2_2.java
@@ -0,0 +1,22 @@
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+        
+        System.out.println("   /\\             /\\");
+        System.out.println("  / \\____________/  \\");
+        System.out.println(" /                   \\");
+        System.out.println("(    /\\         /\\    )");
+        System.out.println("====       v       ====");
+        System.out.println("====== (___|___) ======");
+        System.out.println("   (                 )   ");
+        System.out.println("     (_____________)     ");
+
+        
+        
+        
+        
+        
+        
+    } 
+}
